232ea3532a79f2b65f9f38661aa4a2749e2db8e5
Balram Patidar <pbalram088@gmail.com>
2022-07-20 18:41:22 +0530

    add slider
    slider

--- max/src/component/Pages/Home.jsx
+++ max/src/component/Pages/Home.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React from 'react';
+import style from 'styled-components';
+import { Box } from "@chakra-ui/react";
+import ImageSlider from "../Slider/ImageSlider";
+import { SlideData } from "../Slider/SlideData";
+
+const Banner = style.div`
+  background-color: #303ab2;
+  height: 43px;
+  text-align: center;
+  padding-top: 8px;
+  color: #ffff;
+  
+ 
+ 
+`;
 
 export const Home = () => {
   return (
-    <div>
+ <>  
+ <div>
+      <Banner>
+        <p>
+          Buy 3 @ ₹499/699/999/1499 + Free Shipping on orders above JUST ₹499.
+        </p>
+      </Banner>
+      <Banner>
+        <p>
+          Buy 3 @ ₹499/699/999/1499 + Free Shipping on orders above JUST ₹499.
+        </p>
+      </Banner>
+      <Box w="100%" p={4} color="white">
+        <ImageSlider slides={SlideData} />
+      </Box>
+      </div>
+   </>
 
-      
-    </div>
-  )
+)
 }
