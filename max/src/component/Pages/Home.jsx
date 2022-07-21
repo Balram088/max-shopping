b73f4ac896158afd762408a5e7ae37c1eb5e9745
Balram Patidar <pbalram088@gmail.com>
2022-07-21 19:15:00 +0530

    body
    body

--- max/src/component/Pages/Home.jsx
+++ max/src/component/Pages/Home.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import style from 'styled-components';
-import { Box,Center,Heading } from "@chakra-ui/react";
+import React from "react";
+import style from "styled-components";
+import { Box, Center } from "@chakra-ui/react";
 import ImageSlider from "../Slider/ImageSlider";
 import { SlideData } from "../Slider/SlideData";
 const Banner = style.div`
@@ -16,48 +16,123 @@ const Banner = style.div`
 
 export const Home = () => {
   return (
- <>  
- <div>
-      <Banner>
-        <p>
-          Buy 3 @ ₹499/699/999/1499 + Free Shipping on orders above JUST ₹499.
-        </p>
-      </Banner>
-      <Banner>
-        <p>
-          Buy 3 @ ₹499/699/999/1499 + Free Shipping on orders above JUST ₹499.
-        </p>
-      </Banner>
-      <Banner>
-        <p>
-          Buy 3 @ ₹499/699/999/1499 + Free Shipping on orders above JUST ₹499.
-        </p>
-      </Banner>
-      <Box w="100%" color="white">
-        <ImageSlider slides={SlideData} />
-      </Box>
-      <Box w="100%" mb="50" center>
-      <Center  w="100%" h='100px' color='white'>
-      <img src="https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedAD8AF927.lmsin.net/MAX-Friday/MAX2.O/desktop-department-modularblock-oneBytwo1A-FREESHIPCOMMON-20JULY2022A.png" alt="" />
-
-      </Center>
-      </Box>
-      
+    <>
       <div>
-      <hr />
-     <div>
-      <p style={{textAlign:"left" ,fontSize:"40px", margin:"40px"}}>Style Spotlight</p>
-     </div>
-  
-      
-      <Center h='100%' w='100%' >
-        <img src="https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedAD8AF927.lmsin.net/MAX-Friday/MAX2.O/desktop-department-modularblock-oneBythree-D-Boys-18JULY2022.png" alt="" />
-        </Center> 
-      
+        <Banner>
+          <p>
+            Buy 3 @ ₹499/699/999/1499 + Free Shipping on orders above JUST ₹499.
+          </p>
+        </Banner>
+        <Banner>
+          <p>
+            Buy 3 @ ₹499/699/999/1499 + Free Shipping on orders above JUST ₹499.
+          </p>
+        </Banner>
+        <Banner>
+          <p>
+            Buy 3 @ ₹499/699/999/1499 + Free Shipping on orders above JUST ₹499.
+          </p>
+        </Banner>
+        <Center w="100%" color="white">
+          <ImageSlider slides={SlideData} />
+        </Center>
+        <Box w="100%" mb="50" center>
+          <Center w="100%" h="100px" color="white">
+            <img
+              src="https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedAD8AF927.lmsin.net/MAX-Friday/MAX2.O/desktop-department-modularblock-oneBytwo1A-FREESHIPCOMMON-20JULY2022A.png"
+              alt=""
+            />
+          </Center>
+        </Box>
+
+        <div>
+          <div>
+            <p style={{ textAlign: "left", fontSize: "40px", margin: "40px" }}>
+              Style Spotlight
+            </p>
+          </div>
+
+          <Center h="100%" w="100%">
+            <img
+              src="https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedAD8AF927.lmsin.net/MAX-Friday/MAX2.O/desktop-department-modularblock-oneBythree-D-Boys-18JULY2022.png"
+              alt=""
+            />
+          </Center>
+        </div>
+
+        <div>
+          <div>
+            <p style={{ textAlign: "left", fontSize: "40px", margin: "40px" }}>
+              Customer Favorite!
+            </p>
+          </div>
+
+          <Center h="100%" w="100%">
+            <img
+              src="https://lmsin.net/cdn-cgi/image/w=1232,q=85,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedAD8AF927.lmsin.net/MAX-Friday/MAX2.O/desktop-department-modularblock-oneBythree-A-KIDSFW-19JULY2022.gif"
+              alt=""
+            />
+          </Center>
+        </div>
+
+        <div>
+          <div>
+            <p style={{ textAlign: "left", fontSize: "40px", margin: "40px" }}>
+              Trends
+            </p>
+          </div>
+
+          <Center h="100%" w="100%">
+            <img
+              src="https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedAD8AF927.lmsin.net/MAX-Friday/MAX2.O/desktop-department-10modularblock-oneBythree-A-Boys-18JULY2022.png"
+              alt=""
+            />
+          </Center>
+        </div>
+
+        <div>
+          <Center h="100%" w="100%" mt="50px">
+            <img
+              src="https://lmsin.net/cdn-cgi/image/w=1232,q=60,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedAD8AF927.lmsin.net/MAX-Friday/MAX2.O/desktop-department-11modularblock-oneBythree-A-Boys-18JULY2022.png"
+              alt=""
+            />
+          </Center>
         </div>
         
-      </div>
-   </>
 
-)
-}
+        <div>
+          <div>
+            <p style={{ textAlign: "left", fontSize: "40px", margin: "40px" }}>
+              
+              Connect With Us
+            </p>
+          </div>
+
+          <Center h="100%" w="100%">
+            <img
+              src="https://lmsin.net/cdn-cgi/image/w=1232,q=60,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedAD8AF927.lmsin.net/MAX-Friday/MAX2.O/desktop-department-modularblock8-oneBythree-A-Women-21Oct2020.jpg"
+              alt=""
+            />
+          </Center>
+        </div>
+        <div>
+          <div>
+            <p style={{ textAlign: "left", fontSize: "40px", margin: "40px" }}>
+             
+            Gifting Zone
+            </p>
+          </div>
+
+          <Center>
+            <img
+              height="100%" 
+              width="100%"
+              src="https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/MAX-Friday/MAX2.O/desktop-modularblock14One-Boys-26Oct2021.jpg"
+              alt=""
+            />
+          </Center>
+        </div>
+      </div>
+    </>
+  );
+};
