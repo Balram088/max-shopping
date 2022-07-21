efe6cd90d075104f10bdec55fa5d7ca536584cf2
Balram Patidar <pbalram088@gmail.com>
2022-07-21 22:01:09 +0530

    add main
    main for conflict issue

--- max/src/component/Pages/Home.jsx
+++ max/src/component/Pages/Home.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import style from "styled-components";
-
 import { Box, Center, Spacer, Text } from "@chakra-ui/react";
 
 import ImageSlider from "../Slider/ImageSlider";
 import { SlideData } from "../Data/SlideData";
 import { flexData } from "../Data/HomeData";
-
 
 const Banner = style.div`
   background-color: #303ab2;
@@ -17,16 +15,13 @@ const Banner = style.div`
 `;
 const Body = style.div`
 margin:0px 100px 0px 100px;
-`
-
-
+`;
 
 export const Home = () => {
   return (
     <>
-
       <Body>
-      <div>
+        <div>
           <Banner>
             <p>
               Buy 3 @ ₹499/699/999/1499 + Free Shipping on orders above JUST
@@ -64,7 +59,6 @@ export const Home = () => {
             </Box>
           ))}
         </div>
-
       </Body>
     </>
   );
