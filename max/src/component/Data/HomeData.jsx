b33daff8df74eedc77bc10fdc3ecac5f73424eeb
Balram Patidar <pbalram088@gmail.com>
2022-07-21 21:04:58 +0530

    arrange by datafiles
    arrange by datafiles

---
+++ max/src/component/Data/HomeData.jsx
@@ -0,0 +1,32 @@
+export const flexData=
+  [
+    {
+     tag:"",
+     img:"https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedAD8AF927.lmsin.net/MAX-Friday/MAX2.O/desktop-department-modularblock-oneBytwo1A-FREESHIPCOMMON-20JULY2022A.png"
+    },
+    {
+      tag:"Style Spotlight",
+      img:"https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedAD8AF927.lmsin.net/MAX-Friday/MAX2.O/desktop-department-modularblock-oneBythree-D-Boys-18JULY2022.png"
+     },
+     {
+       tag:"Customer Favorite!",
+       img:"https://lmsin.net/cdn-cgi/image/w=1232,q=85,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedAD8AF927.lmsin.net/MAX-Friday/MAX2.O/desktop-department-modularblock-oneBythree-A-KIDSFW-19JULY2022.gif"
+      },
+      {
+        tag:"Trends",
+        img:"https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedAD8AF927.lmsin.net/MAX-Friday/MAX2.O/desktop-department-10modularblock-oneBythree-A-Boys-18JULY2022.png"
+       },
+       {
+         tag:"",
+         img:"https://lmsin.net/cdn-cgi/image/w=1232,q=60,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedAD8AF927.lmsin.net/MAX-Friday/MAX2.O/desktop-department-11modularblock-oneBythree-A-Boys-18JULY2022.png"
+        },
+        {
+          tag:"Connect With Us",
+          img:"https://lmsin.net/cdn-cgi/image/w=1232,q=60,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedAD8AF927.lmsin.net/MAX-Friday/MAX2.O/desktop-department-modularblock8-oneBythree-A-Women-21Oct2020.jpg"
+         },
+         {
+           tag:"Gifting Zone",
+           img:"https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/MAX-Friday/MAX2.O/desktop-modularblock14One-Boys-26Oct2021.jpg"
+          }
+    
+  ]
--- max/src/component/Pages/Home.jsx
+++ max/src/component/Pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import style from "styled-components";
-import { Box, Center } from "@chakra-ui/react";
+import { Box, Center, Spacer, Text } from "@chakra-ui/react";
 import ImageSlider from "../Slider/ImageSlider";
-import { SlideData } from "../Slider/SlideData";
+import { SlideData } from "../Data/SlideData";
+import {flexData} from '../Data/HomeData';
+
 const Banner = style.div`
   background-color: #303ab2;
   height: 50px;
@@ -10,10 +12,16 @@ const Banner = style.div`
   padding-top: 20px;
   color: #ffff;
 `;
+const Body = style.div`
+margin:0px 100px 0px 100px;
+`
+
+
 
 export const Home = () => {
   return (
     <>
+    <Body>
       <div>
         <Banner>
           <p>
@@ -30,106 +38,32 @@ export const Home = () => {
             Buy 3 @ ₹499/699/999/1499 + Free Shipping on orders above JUST ₹499.
           </p>
         </Banner>
+      
         <Center w="100%" color="white">
           <ImageSlider slides={SlideData} />
         </Center>
-        <Box w="100%" mb="50" center>
-          <Center w="100%" h="100px" color="white">
-            <img
-              src="https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedAD8AF927.lmsin.net/MAX-Friday/MAX2.O/desktop-department-modularblock-oneBytwo1A-FREESHIPCOMMON-20JULY2022A.png"
-              alt=""
-            />
-          </Center>
-        </Box>
-
-        <div>
-          <div>
-            <p style={{ textAlign: "left", fontSize: "60px", margin: "60px" }}>
-              Style Spotlight
-            </p>
-          </div>
-
-          <Center h="100%" w="100%">
-            <img
-              src="https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedAD8AF927.lmsin.net/MAX-Friday/MAX2.O/desktop-department-modularblock-oneBythree-D-Boys-18JULY2022.png"
-              alt=""
-            />
-          </Center>
-        </div>
-
-        <div>
-          <div>
-            <p style={{ textAlign: "left", fontSize: "60px", margin: "60px" }}>
-              Customer Favorite!
-            </p>
-          </div>
-
-          <Center h="100%" w="100%">
-            <img
-              src="https://lmsin.net/cdn-cgi/image/w=1232,q=85,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedAD8AF927.lmsin.net/MAX-Friday/MAX2.O/desktop-department-modularblock-oneBythree-A-KIDSFW-19JULY2022.gif"
-              alt=""
-            />
-          </Center>
-        </div>
-
-        <div>
-          <div>
-            <p style={{ textAlign: "left", fontSize: "60px", margin: "60px" }}>
-              Trends
-            </p>
-          </div>
-
-          <Center h="100%" w="100%">
-            <img
-              src="https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedAD8AF927.lmsin.net/MAX-Friday/MAX2.O/desktop-department-10modularblock-oneBythree-A-Boys-18JULY2022.png"
-              alt=""
-            />
-          </Center>
-        </div>
-
-        <div>
-          <Center h="100%" w="100%" mt="50px">
-            <img
-              src="https://lmsin.net/cdn-cgi/image/w=1232,q=60,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedAD8AF927.lmsin.net/MAX-Friday/MAX2.O/desktop-department-11modularblock-oneBythree-A-Boys-18JULY2022.png"
-              alt=""
-            />
-          </Center>
-        </div>
         
-
-        <div>
-          <div>
-            <p style={{ textAlign: "left", fontSize: "60px", margin: "60px" }}>
-              
-              Connect With Us
-            </p>
-          </div>
-
+        {
+         flexData.map((e)=>(
+          <Box>
           <Center h="100%" w="100%">
-            <img
-              src="https://lmsin.net/cdn-cgi/image/w=1232,q=60,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedAD8AF927.lmsin.net/MAX-Friday/MAX2.O/desktop-department-modularblock8-oneBythree-A-Women-21Oct2020.jpg"
-              alt=""
-            />
+          <Text fontSize='6xl'  m='40px' >{e.tag}</Text>
+          <Spacer/>
           </Center>
-        </div>
-        <div>
-          <div>
-            <p style={{ textAlign: "left", fontSize: "60px", margin: "60px" }}>
-             
-            Gifting Zone
-            </p>
-          </div>
+          <hr />
+           <Center h="100%" w="100%">
+           <img
+             src={e.img}
+             alt=""
+           />
+         </Center>
+         </Box>
+         )) 
+        }
 
-          <Center>
-            <img
-              height="100%" 
-              width="100%"
-              src="https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/MAX-Friday/MAX2.O/desktop-modularblock14One-Boys-26Oct2021.jpg"
-              alt=""
-            />
-          </Center>
-        </div>
-      </div>
+
+      </div>       
+       </Body>
     </>
   );
 };
