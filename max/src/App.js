9b6145c38b060ee124436f3a587a24428372930f
Balram Patidar <pbalram088@gmail.com>
2022-07-22 13:01:45 +0530

    footer
    footer

--- max/src/App.js
+++ max/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import Footer from './component/Pages/Footer/Footer';
 import MainRoutes from './component/Routes/MainRoutes';
 
 function App() {
   return (
     <div className="App">
        <MainRoutes/>
+       <br />
+       <br />
+       <hr />
+       <Footer/>
     </div>
   );
 }
